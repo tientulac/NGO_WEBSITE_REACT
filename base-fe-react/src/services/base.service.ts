47c22b2e7484cc2f801868baff69ab437cf87dd4
minhnv1 <99402972+MustBmaster@users.noreply.github.com>
2025-07-12 15:06:18 +0700

    Update base.service.ts

--- base-fe-react/src/services/base.service.ts
+++ base-fe-react/src/services/base.service.ts
@@ -1,27 +1,24 @@
-import axios from "axios";
-import { ResponseEntity } from "../entities/response.entity";
+import axios from 'axios'
+import { ResponseEntity } from '../entities/response.entity'
 
-const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:3000";
+const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:3000'
 
 const api = axios.create({
-  headers: {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${localStorage?.getItem("token") || ""}`,
-  },
-  timeout: 10000,
-});
+	headers: {
+		'Content-Type': 'application/json',
+		Authorization: `Bearer ${localStorage?.getItem('token') || ''}`,
+	},
+	timeout: 10000,
+})
 
 export class BaseService {
-  // GET request
-  public static async getList<T>(url: string): Promise<ResponseEntity<T>> {
-    return (await api.get<ResponseEntity<T>>(apiUrl + url)).data;
-  }
+	// GET request
+	public static async getList<T>(url: string): Promise<ResponseEntity<T>> {
+		return (await api.get<ResponseEntity<T>>(apiUrl + url)).data
+	}
 
-  // POST request
-  public static async post<T>(
-    url: string,
-    data: T
-  ): Promise<ResponseEntity<T>> {
-    return (await api.post<ResponseEntity<T>>(apiUrl + url, data)).data;
-  }
+	// POST request
+	public static async post<T>(url: string, data: T): Promise<ResponseEntity<T>> {
+		return (await api.post<ResponseEntity<T>>(apiUrl + url, data)).data
+	}
 }

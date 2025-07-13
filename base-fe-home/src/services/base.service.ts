9e24e3e5127643b320cda7d6ecc8e452a29184c7
minhnv1 <99402972+MustBmaster@users.noreply.github.com>
2025-07-13 10:54:49 +0700

    Base FE Vite mirgrate

--- base-fe-home/src/services/base.service.ts
+++ base-fe-home/src/services/base.service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { ResponseEntity } from "../entities/response.entity";
 
-const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:3000";
+const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:3000'
 
 const api = axios.create({
   headers: {
